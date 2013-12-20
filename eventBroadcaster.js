c6b0e805140190a9d547872343786ef77101fa2a
Erick Mendoza <contact@odiseo.net>
2013-12-20 13:16:46 -0600

    First stable 1.0 version
    Adds a static jQuery function $.emitCustomEvent to easily broadcast a custom event

--- eventBroadcaster.js
+++ eventBroadcaster.js
@@ -1,5 +1,17 @@
-(function($) {
-  "use strict";
+/*!
+ * jQuery Event Broadcaster v1.0
+ * Simple way to bradcast and emit customEvent
+ * objects to all the DOM
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2013 Erick Mendoza
+ * Released under the MIT license
+ */
+(function ($, document, undefined) {
+  /**
+   * @param {Object} any Event object
+   * https://developer.mozilla.org/en/docs/Web/API/Event
+   */
   $.fn.broadcastEvent = function( eventObj ){
     var trigger = function(){
       if (this.dispatchEvent) {
@@ -11,4 +23,30 @@
     return this;
   };
 
-}(jQuery));
+  /**
+   * A wrapper for broadcastEvent to easily emit CustomEvent
+   * @param {string} eventType A string containing a JavaScript event type,
+   * such as click or submit, OR a custom event name.
+   * @param {Object} dataObj In case of custom event, this object will be
+   * used as 'details' in CustomEvent property
+   */
+  $.emitCustomEvent = function(eventType, dataObj){
+    var globalBroascaster = $('body'),
+      emittedEvent;
+
+    if (typeof eventType !== 'string'){
+      throw 'First argument must be an event name';
+    }
+
+    emittedEvent = new window.CustomEvent(eventType,
+      {
+        detail: dataObj,
+        bubbles: false
+      }
+    );
+
+    globalBroascaster.broadcastEvent(emittedEvent);
+  };
+
+
+})(jQuery, document);
